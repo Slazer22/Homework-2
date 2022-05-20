0e2181c765adfe895add187a80202221db1e3f77
Slazer22 <48864818+Slazer22@users.noreply.github.com>
2022-05-19 19:48:25 -0500

    Add project files.

---
+++ Practice2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+public class EmailValidator
+{
+	public static void Main(string[] args)
+	{
+		//String elligible characters.
+		string regex = @"^([\w.-]+)@([\w-]+)((.(\w){2,})+)$";
+
+		while (true)
+		{
+			Console.Write("Enter an email address: ");
+
+			string? input = Console.ReadLine();
+			bool regexMatch = false;
+
+			regexMatch = Regex.IsMatch(input, regex);
+
+			MailAddress? address;
+
+			if (!MailAddress.TryCreate(input, result: out address))
+			{
+				Console.WriteLine($"{input} is not a valid email address");
+				if (regexMatch)
+				{
+					Console.WriteLine("But the regex check passed, so it must be garbage");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Finally");
+				return;
+			}
+
+			Console.WriteLine("No soup for you! try again");
+		}
+	}
+}
